83730f8a2c059ab289ef76ae24c142bbf4dfe8d7
Philip <philip.nguyen@sjsu.edu>
2021-12-09 23:26:56 -0800

    first commit

---
+++ src/assets/taskData.js
@@ -0,0 +1,17 @@
+export const taskData = [
+    {
+        title: "Learn React with Redux"
+    },
+    {
+        title: "Study Ray Tracing"
+    },
+    {
+        title: "Learn .NET Core for Backend"
+    },
+    {
+        title: "Study Korean"
+    },
+    {
+        title: "Study Vietnamese"
+    }
+];
